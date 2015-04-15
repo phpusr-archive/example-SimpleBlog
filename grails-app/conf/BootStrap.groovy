7967ed9fdf0eb5c4e0a0e39094f7ac5a951a9302
phpusr <phpusr@gmail.com>
2015-04-15 12:03:09 +0500

    add: Данные для инициализации

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,20 @@
+import org.phpusr.simpleblog.Role
+import org.phpusr.simpleblog.User
+import org.phpusr.simpleblog.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+        def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+
+        def adminUser = User.findByUsername('admin') ?: new User(username: 'admin', password: '123', enabled: true).save(failOnError: true)
+
+        if (!adminUser.authorities.contains(adminRole)) {
+            UserRole.create(adminUser, adminRole)
+        }
+
     }
     def destroy = {
     }
