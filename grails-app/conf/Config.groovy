e9f2c0c72d0ee9c502fe43f240a063a86180954a
phpusr <phpusr@gmail.com>
2015-04-15 10:57:01 +0500

    add: Пользователи и роли

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'org.phpusr.simpleblog.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'org.phpusr.simpleblog.UserRole'
+grails.plugin.springsecurity.authority.className = 'org.phpusr.simpleblog.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/assets/**':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+
