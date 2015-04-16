d65e3e18d36b6ac7711c0ccf8fee90302fef5ebf
phpusr <phpusr@gmail.com>
2015-04-16 16:51:55 +0500

    change: Язык по умолчанию английский

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,11 @@
 // Place your Spring DSL code here
 beans = {
+    /**
+     * Язык по умолчанию
+     * Параметр для переключения по ссылке: lang=en
+     */
+    localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
+        defaultLocale = new Locale("en","EN")
+        java.util.Locale.setDefault(defaultLocale)
+    }
 }
